0f2ba54b1d9db2e0210f8779a0b610f1a833a8fc
Win Le <nguyenwinle@gmail.com>
2018-07-02 16:12:50 -0700

    edit js

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
      
-    var questions = {
+    var questions = [{
+        question: "Which of these players never scored at least 2500 points in a season?",
+        choices: ["Kevin Durant", "Rick Barry", "Lebron James", "Bob Macadoo"],
+        images: ["../images/Rajah.gif"],
+        correct: 2
+    }, {
+        question: "In 1980, Oscar Robertson was the NBA's all time leader in assists, who was 2nd?",
+        choices: ["Michael Jordan", "Scottie Pippen", "Larry Bird", "John Stockton"],
+        correct: 2
 
-    };
+    }, {
+        question: "Which of these players has never had a season in which they averaged at least 30 PPG on at least 55 FG%?",
+        choices: ["Michael Jordan", "Kareem Abdul Jabar", "Adrian", "Karl Malone"],
+        correct: 3
+    }];
 
 // Variable showImage will hold the setInterval when we start the slideshow
     var showQuestion;
@@ -12,6 +24,7 @@ $(document).ready(function() {
     var wrong = 0;
     var unanswered = 0;
     var percentage = (right / 10 * 100) + '%';
+    var images;
 
 
     // TODO: Use jQuery to run "startTrivia" when we click the "start" button.
@@ -24,6 +37,7 @@ $(document).ready(function() {
     // This function will replace display whatever question it's given
     function displayQuestion() {
         // generate questions
+
     }
 
     function nextQuestion() {
